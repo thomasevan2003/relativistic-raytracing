de2c35f6bc074e02fd1e82ed594c11291556b0e1
Evan Thomas <thomasevan2003@gmail.com>
2025-08-23 16:31:32 -0400

    window creation

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 Shader visualization of gravitational lensing around a black hole, written in C++.
 
 ## Dependencies
-Dear ImGui
+- Dear ImGui
+- glfw  
+- glad  
+- OpenGL
 
-## Third Party Libraries
-This project uses the Dear ImGui library, licensed under the MIT License. 
-Copyright (c) 2014-2025 Omar Cornut
-See third-party/imgui/LICENSE.txt for details.
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <GLFW/glfw3.h>
+
+
+void run() {
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(900, 600, "RelativisticRaytracer", NULL, NULL);
+	
+	if (!window) {
+		glfwTerminate();
+		std::cerr << "Failed to create window" << std::endl;
+		return;
+	}
+	
+	glfwMakeContextCurrent(window);
+	
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
 
 int main() {
-	std::cout << "END OF MAIN" << std::endl;
+	run();
+	std::cout << "END OF PROGRAM" << std::endl;
 }
